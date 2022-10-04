cb20f5c66ef4ac2f7a9eb741200ff3e807357a87
KitFieldhouse <kit.fieldhouse@gmail.com>
2022-10-04 18:34:34 -0500

    Grab ELog Data from current time to 3 days ago

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
 
-
+var threeDays = 3*86400000; // number of milliseconds in three days
 
 
 
 function getData(logs)
 {
+
+	var sDate = new Date();
+	var sYear = string(sDate.getFullYear());
+	var sMonth = string(int(sDate.getMonth()) + 1);
+	var sDay = string(sDate.getDate());
+	var sHour = string(sDate.getHours());
+	var sMin = string(sDate.getMinutes());
+
+	var eDate = new Date(sDate.getTime() - threeDays);
+	var eYear = string(sDate.getFullYear());
+	var eMonth = string(int(sDate.getMonth()) + 1);
+	var eDay = string(sDate.getDate());
+
+	var root = "https://www-bd.fnal.gov/Elog/?";
+	var logQueryString = "orLogName=";
+	var dateQueryString = "startDate=" + sYear + "-" + sMonth + "-" + sDay + "+" + sHour + "%3A" + sMin + "%3A00" +
+						+ "&endDate=" +  eYear + "-" + eMonth + "-" + eDay + "+00%3A00%3A00";
 	
 	for(a in logs)
 	{
-		var link = "https://www-bd.fnal.gov/Elog/?orLogName=" + logs[a];
+		var link = root + logQueryString + logs[a] + "&" + dateQueryString;
 		console.log(link);
 		
 		const req = new XMLHttpRequest();
